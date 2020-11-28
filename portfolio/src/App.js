2f4591d0a8ce76aadf34ba643465e19a9a0f74d6
imjchiang <jchiang296@gmail.com>
2020-11-27 17:57:57 -0800

    [App.js] add nav [main.css] style the nav

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 import './css/App.css';
 import './css/main.css';
 
@@ -6,6 +8,11 @@ function App()
     return (
         <div>
             <h1 className="title">Joshua Chiang</h1>
+            <span className="navbar">
+                <p className="nav">About Me</p>
+                <p className="nav">My Projects</p>
+                <p className="nav">Contact Info</p>
+            </span>
             <hr />
         </div>
     );
